db4794499fdc2293baffaa9ef998351250463127
Martin Begleiter <martin.begleiter@tunstallnordic.com>
2016-12-06 22:40:18 +0100

    Added methods in datamanager to create Article

--- app/app/src/main/java/com/blackwabi/barcodelist/data/DataManager.java
+++ app/app/src/main/java/com/blackwabi/barcodelist/data/DataManager.java
@@ -1,8 +1,10 @@
 package com.blackwabi.barcodelist.data;
 
-import android.content.Context;
+import com.blackwabi.barcodelist.data.model.Article;
 
 import javax.inject.Inject;
+
+import io.realm.Realm;
 
 /**
  * Created by martinbegleiter on 27/11/16.
@@ -10,7 +12,18 @@ import javax.inject.Inject;
 
 public class DataManager {
 
+    private final Realm mRealm;
+
     @Inject
-    public DataManager(Context context) {
+    public DataManager() {
+        mRealm = Realm.getDefaultInstance();
+    }
+
+    public void addArticle(String articleName, String articleCode) {
+        final Article article = new Article();
+        article.articleName = articleName;
+        article.articleCode = articleCode;
+
+        mRealm.executeTransaction(realm -> realm.copyToRealmOrUpdate(article));
     }
 }
