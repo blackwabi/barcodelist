b2fc4309126ae84d733712093a54e77f22647c74
Martin Begleiter <martin.begleiter@tunstallnordic.com>
2016-12-06 22:39:08 +0100

    Added Article realm class

---
+++ app/app/src/main/java/com/blackwabi/barcodelist/data/model/Article.java
@@ -0,0 +1,15 @@
+package com.blackwabi.barcodelist.data.model;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+/**
+ * Created by martinbegleiter on 06/12/16.
+ */
+
+public class Article extends RealmObject {
+    public String articleName;
+
+    @PrimaryKey
+    public String articleCode;
+}
