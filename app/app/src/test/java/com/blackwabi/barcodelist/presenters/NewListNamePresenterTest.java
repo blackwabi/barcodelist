8100251af18a6611944013dfc045c3760b433e9d
Martin Begleiter <martin.begleiter@tunstallnordic.com>
2017-01-04 10:36:36 +0100

    Adding tests for creating a new list

---
+++ app/app/src/test/java/com/blackwabi/barcodelist/presenters/NewListNamePresenterTest.java
@@ -0,0 +1,48 @@
+package com.blackwabi.barcodelist.presenters;
+
+import com.blackwabi.barcodelist.Navigator;
+import com.blackwabi.barcodelist.fragments.NewListNameFragment;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Created by martinbegleiter on 26/12/16.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class NewListNamePresenterTest {
+
+    private static final String LIST_NAME = "LIST_NAME";
+
+    @Mock
+    Navigator mNavigator;
+
+    @Mock
+    NewListNameFragment mFragment;
+
+    NewListNamePresenter mPresenter;
+
+    @Before
+    public void setUp() throws Exception {
+        mPresenter = new NewListNamePresenter(mNavigator);
+        mPresenter.setFragment(mFragment);
+    }
+
+    @Test
+    public void onSaveListNameClick() throws Exception {
+        mPresenter.onSaveListNameClick(LIST_NAME);
+        verify(mNavigator, times(1)).goToNewList(LIST_NAME);
+    }
+
+    @Test
+    public void onCancelClick() throws Exception {
+        mPresenter.onCancelClick();
+        verify(mNavigator, times(1)).goToLists();
+    }
+}
