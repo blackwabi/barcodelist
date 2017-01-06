c779b2064fa9c2fbd3d52e510cc38cb4ad978287
Martin Begleiter <martin.begleiter@tunstallnordic.com>
2017-01-07 00:05:13 +0100

    Adding choosearticlepresenter tests

---
+++ app/app/src/test/java/com/blackwabi/barcodelist/presenters/ChooseArticlePresenterTest.java
@@ -0,0 +1,62 @@
+package com.blackwabi.barcodelist.presenters;
+
+import com.blackwabi.barcodelist.Navigator;
+import com.blackwabi.barcodelist.data.DataManager;
+import com.blackwabi.barcodelist.data.model.Article;
+import com.blackwabi.barcodelist.fragments.ChooseArticleFragment;
+import com.blackwabi.barcodelist.fragments.ShowArticlesFragment;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by martinbegleiter on 26/12/16.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ChooseArticlePresenterTest {
+
+    private static final String LIST_NAME = "list_name";
+    @Mock
+    Navigator mNavigator;
+
+    @Mock
+    DataManager mDataManager;
+
+    @Mock
+    ChooseArticleFragment mFragment;
+
+    ChooseArticlePresenter mPresenter;
+
+    @Before
+    public void setUp() throws Exception {
+        mPresenter = new ChooseArticlePresenter(mNavigator, mDataManager);
+        mPresenter.setListName(LIST_NAME);
+    }
+
+    @Test
+    public void onItemClicked() {
+        Article article = new Article();
+        mPresenter.onItemClicked(0, article);
+        verify(mDataManager, times(1)).addExistingArticleToExistingList(LIST_NAME, article);
+        verify(mNavigator, times(1)).goBack();
+    }
+
+    @Test
+    public void setFragment() {
+        List<Article> articles = new ArrayList<>();
+        when(mDataManager.getArticles()).thenReturn(articles);
+        mPresenter.setFragment(mFragment);
+        verify(mDataManager, times(1)).getArticles();
+        verify(mFragment, times(1)).setList(articles);
+    }
+}
