58a234bd1cd52b92d18658a7662b5c0883c016b4
Martin Begleiter <martin.begleiter@tunstallnordic.com>
2017-03-29 20:00:10 +0200

    Added onetime initialization of database method

--- app/app/src/main/java/com/blackwabi/barcodelist/presenters/ShowArticlesPresenter.java
+++ app/app/src/main/java/com/blackwabi/barcodelist/presenters/ShowArticlesPresenter.java
@@ -22,6 +22,16 @@ public class ShowArticlesPresenter extends BasePresenter<ShowArticlesFragment> {
     public ShowArticlesPresenter(Navigator navigator, DataManager dataManager) {
         mNavigator = navigator;
         mDataManager = dataManager;
+        //createArticles();
+    }
+
+    private void createArticles() {
+        mDataManager.addArticle("Heinz Ketchup", "111");
+        mDataManager.addArticle("Korv", "222");
+        mDataManager.addArticle("Bröd", "333");
+        mDataManager.addArticle("Senap", "444");
+        mDataManager.addArticle("Choklad", "555");
+        mDataManager.addArticle("Mjölk", "555");
     }
 
     @Override
